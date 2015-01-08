90eea5071c295947de273e106fde4a8359870cc8
layo <andrievskiy.r@gmail.com>
2015-01-08 21:56:34 +0200

    Adding Logback

--- src/main/java/org/layo/todolist/dao/impl/TodoListDAOImpl.java
+++ src/main/java/org/layo/todolist/dao/impl/TodoListDAOImpl.java
@@ -4,6 +4,8 @@ import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.layo.todolist.dao.TodoListDAO;
 import org.layo.todolist.entity.TodoItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,28 +17,34 @@ import java.util.List;
 @Repository
 public class TodoListDAOImpl implements TodoListDAO {
 
+    private static final Logger logger = LoggerFactory.getLogger(TodoListDAOImpl.class);
+
     @Autowired
     private SessionFactory sessionFactory;
 
     @Override
     public void addTodoItem(TodoItem todoItem) {
+        logger.info("Add TodoItem");
         sessionFactory.getCurrentSession().save(todoItem);
     }
 
     @Override
     public void updateTodoItem(TodoItem todoItem) {
+        logger.info("Update TodoItem");
         sessionFactory.getCurrentSession().update(todoItem);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<TodoItem> getAllTodoItems() {
+        logger.info("Get TodoList");
         Criteria criteria = sessionFactory.getCurrentSession().createCriteria(TodoItem.class);
         return criteria.list();
     }
 
     @Override
     public void deleteTodoItem(int id) {
+        logger.info("Delete TodoItem");
         TodoItem todoItem = new TodoItem(id);
         sessionFactory.getCurrentSession().delete(todoItem);
     }
