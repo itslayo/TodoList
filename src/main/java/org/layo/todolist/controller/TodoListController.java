6c8749510ec48c9d1786bd87846d5e27e325e176
layo <andrievskiy.r@gmail.com>
2015-01-08 20:41:29 +0200

    Delete empty constructor

--- src/main/java/org/layo/todolist/controller/TodoListController.java
+++ src/main/java/org/layo/todolist/controller/TodoListController.java
@@ -17,31 +17,27 @@ public class TodoListController {
     @Autowired
     TodoListService todoListService;
 
-    public TodoListController() {}
-
-    @RequestMapping(value = "/todos", method = RequestMethod.GET, headers="Accept=application/json")
+    @RequestMapping(value = "/todos", method = RequestMethod.GET, headers = "Accept=application/json")
     @SuppressWarnings("unchecked")
-    public List<TodoItem> getAllTasks()
-    {
+    public List<TodoItem> getAllTasks() {
         List<TodoItem> tasks;
         tasks = todoListService.getAllTodoItems();
         return tasks;
     }
 
-    @RequestMapping(value = "/todos", method = RequestMethod.POST, headers="Accept=application/json")
-    public  TodoItem addTask(@RequestBody TodoItem todoItem)
-    {
+    @RequestMapping(value = "/todos", method = RequestMethod.POST, headers = "Accept=application/json")
+    public TodoItem addTask(@RequestBody TodoItem todoItem) {
         todoListService.addTodoItem(todoItem);
         return todoItem;
     }
 
-    @RequestMapping(value = "/todos/{id}", method = RequestMethod.DELETE, headers="Accept=application/json")
-     public  void deleteTask(@PathVariable int id) {
+    @RequestMapping(value = "/todos/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public void deleteTask(@PathVariable int id) {
         todoListService.deleteTodoItem(id);
     }
 
-    @RequestMapping(value = "/todos", method = RequestMethod.PUT, headers="Accept=application/json")
-    public  void updateTask(@RequestBody TodoItem todoItem) {
+    @RequestMapping(value = "/todos", method = RequestMethod.PUT, headers = "Accept=application/json")
+    public void updateTask(@RequestBody TodoItem todoItem) {
         todoListService.updateTodoItem(todoItem);
     }
 }
